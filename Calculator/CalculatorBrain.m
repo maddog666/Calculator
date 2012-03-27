5fdb87ea352d8ef11dd2940dc5290e93ff14e581
William Hu <hu.will38@gmail.com>
2012-03-27 12:43:33 +1100

    first commit

---
+++ Calculator/CalculatorBrain.m
@@ -0,0 +1,13 @@
+//
+//  CalculatorBrain.m
+//  Calculator
+//
+//  Created by William Hu on 26/03/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "CalculatorBrain.h"
+
+@implementation CalculatorBrain
+
+@end
